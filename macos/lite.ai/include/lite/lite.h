5cf0036b854e6a5b491c6a5d0fb393b347fdad0d
DefTruth <qiustudent_r@163.com>
2021-07-17 17:26:08 +0800

    update liblite.ai.0.0.1.dylib

---
+++ macos/lite.ai/include/lite/lite.h
@@ -0,0 +1,489 @@
+//
+// Created by DefTruth on 2021/3/14.
+//
+
+#ifndef LITE_AI_LITE_H
+#define LITE_AI_LITE_H
+
+#include "backend.h"
+
+// ENABLE_ONNXRUNTIME
+#ifdef ENABLE_ONNXRUNTIME
+
+#include "ort/core/ort_core.h"
+#include "ort/core/ort_utils.h"
+#include "ort/cv/age_googlenet.h"
+#include "ort/cv/arcface_resnet.h"
+#include "ort/cv/colorizer.h"
+#include "ort/cv/deeplabv3_resnet101.h"
+#include "ort/cv/densenet.h"
+#include "ort/cv/efficientnet_lite4.h"
+#include "ort/cv/emotion_ferplus.h"
+#include "ort/cv/fast_style_transfer.h"
+#include "ort/cv/fcn_resnet101.h"
+#include "ort/cv/fsanet.h"
+#include "ort/cv/gender_googlenet.h"
+#include "ort/cv/ghostnet.h"
+#include "ort/cv/hardnet.h"
+#include "ort/cv/ibnnet.h"
+#include "ort/cv/mobilenetv2.h"
+#include "ort/cv/pfld.h"
+#include "ort/cv/resnet.h"
+#include "ort/cv/resnext.h"
+#include "ort/cv/shufflenetv2.h"
+#include "ort/cv/ssd.h"
+#include "ort/cv/ssd_mobilenetv1.h"
+#include "ort/cv/ssrnet.h"
+#include "ort/cv/subpixel_cnn.h"
+#include "ort/cv/tiny_yolov3.h"
+#include "ort/cv/ultraface.h"
+#include "ort/cv/vgg16_age.h"
+#include "ort/cv/vgg16_gender.h"
+#include "ort/cv/yolov3.h"
+#include "ort/cv/yolov4.h"
+#include "ort/cv/yolov5.h"
+#include "ort/cv/glint_cosface.h"
+#include "ort/cv/glint_partial_fc.h"
+#include "ort/cv/facenet.h"
+#include "ort/cv/focal_arcface.h"
+#include "ort/cv/focal_asia_arcface.h"
+#include "ort/cv/tencent_cifp_face.h"
+#include "ort/cv/tencent_curricular_face.h"
+#include "ort/cv/center_loss_face.h"
+#include "ort/cv/sphere_face.h"
+
+#endif
+
+// ENABLE_MNN
+#ifdef ENABLE_MNN
+#endif
+
+// ENABLE_NCNN
+#ifdef ENABLE_NCNN
+#endif
+
+// Default Engine ONNXRuntime
+namespace lite
+{
+  namespace cv
+  {
+#ifdef BACKEND_ONNXRUNTIME
+    namespace utils = ortcv::utils;
+    namespace types = ortcv::types;
+#endif
+
+#ifdef BACKEND_ONNXRUNTIME
+    typedef ortcv::FSANet _FSANet;
+    typedef ortcv::PFLD _PFLD;
+    typedef ortcv::UltraFace _UltraFace;
+    typedef ortcv::AgeGoogleNet _AgeGoogleNet;
+    typedef ortcv::GenderGoogleNet _GenderGoogleNet;
+    typedef ortcv::EmotionFerPlus _EmotionFerPlus;
+    typedef ortcv::VGG16Age _VGG16Age;
+    typedef ortcv::VGG16Gender _VGG16Gender;
+    typedef ortcv::SSRNet _SSRNet;
+    typedef ortcv::FastStyleTransfer _FastStyleTransfer;
+    typedef ortcv::ArcFaceResNet _ArcFaceResNet;
+    typedef ortcv::Colorizer _Colorizer;
+    typedef ortcv::SubPixelCNN _SubPixelCNN;
+    typedef ortcv::YoloV4 _YoloV4;
+    typedef ortcv::YoloV3 _YoloV3;
+    typedef ortcv::YoloV5 _YoloV5;
+    typedef ortcv::EfficientNetLite4 _EfficientNetLite4;
+    typedef ortcv::ShuffleNetV2 _ShuffleNetV2;
+    typedef ortcv::TinyYoloV3 _TinyYoloV3;
+    typedef ortcv::SSD _SSD;
+    typedef ortcv::SSDMobileNetV1 _SSDMobileNetV1;
+    typedef ortcv::DeepLabV3ResNet101 _DeepLabV3ResNet101;
+    typedef ortcv::DenseNet _DenseNet;
+    typedef ortcv::FCNResNet101 _FCNResNet101;
+    typedef ortcv::GhostNet _GhostNet;
+    typedef ortcv::HdrDNet _HdrDNet;
+    typedef ortcv::IBNNet _IBNNet;
+    typedef ortcv::MobileNetV2 _MobileNetV2;
+    typedef ortcv::ResNet _ResNet;
+    typedef ortcv::ResNeXt _ResNeXt;
+    typedef ortcv::GlintCosFace _GlintCosFace;
+    typedef ortcv::GlintPartialFC _GlintPartialFC;
+    typedef ortcv::FaceNet _FaceNet;
+    typedef ortcv::FocalArcFace _FocalArcFace;
+    typedef ortcv::FocalAsiaArcFace _FocalAsiaArcFace;
+    typedef ortcv::TencentCifpFace _TencentCifpFace;
+    typedef ortcv::TencentCurricularFace _TencentCurricularFace;
+    typedef ortcv::CenterLossFace _CenterLossFace;
+    typedef ortcv::SphereFace _SphereFace;
+#endif
+
+    // 1. classification
+    namespace classification
+    {
+#ifdef BACKEND_ONNXRUNTIME
+      typedef _EfficientNetLite4 EfficientNetLite4;
+      typedef _ShuffleNetV2 ShuffleNetV2;
+      typedef _DenseNet DenseNet;
+      typedef _GhostNet GhostNet;
+      typedef _HdrDNet HdrDNet;
+      typedef _IBNNet IBNNet;
+      typedef _MobileNetV2 MobileNetV2;
+      typedef _ResNet ResNet;
+      typedef _ResNeXt ResNeXt;
+#endif
+    }
+
+    // 2. general object detection
+    namespace detection
+    {
+#ifdef BACKEND_ONNXRUNTIME
+      typedef _YoloV3 YoloV3;
+      typedef _YoloV4 YoloV4;
+      typedef _YoloV5 YoloV5;
+      typedef _TinyYoloV3 TinyYoloV3;
+      typedef _SSD SSD;
+      typedef _SSDMobileNetV1 SSDMobileNetV1;
+#endif
+    }
+    // 3. face detection & facial attributes detection
+    namespace face
+    {
+#ifdef BACKEND_ONNXRUNTIME
+      typedef _FSANet FSANet; // head pose estimation.
+      typedef _UltraFace UltraFace;  // face detection.
+      typedef _PFLD PFLD; // facial landmarks detection.
+      typedef _AgeGoogleNet AgeGoogleNet; // age estimation
+      typedef _GenderGoogleNet GenderGoogleNet; // gender estimation
+      typedef _VGG16Age VGG16Age; // age estimation
+      typedef _VGG16Gender VGG16Gender; // gender estimation
+      typedef _EmotionFerPlus EmotionFerPlus; // emotion detection
+      typedef _SSRNet SSRNet; // age estimation
+#endif
+      namespace detect
+      {
+#ifdef BACKEND_ONNXRUNTIME
+        typedef _UltraFace UltraFace;  // face detection.
+#endif
+      }
+
+      namespace align
+      {
+#ifdef BACKEND_ONNXRUNTIME
+        typedef _PFLD PFLD; // facial landmarks detection.
+#endif
+      }
+
+      namespace pose
+      {
+#ifdef BACKEND_ONNXRUNTIME
+        typedef _FSANet FSANet; // head pose estimation.
+#endif
+      }
+
+      namespace attr
+      {
+#ifdef BACKEND_ONNXRUNTIME
+        typedef _AgeGoogleNet AgeGoogleNet; // age estimation
+        typedef _GenderGoogleNet GenderGoogleNet; // gender estimation
+        typedef _VGG16Age VGG16Age; // age estimation
+        typedef _VGG16Gender VGG16Gender; // gender estimation
+        typedef _EmotionFerPlus EmotionFerPlus; // emotion detection
+        typedef _SSRNet SSRNet; // age estimation
+#endif
+      }
+
+    }
+    // 4. face recognition
+    namespace faceid
+    {
+#ifdef BACKEND_ONNXRUNTIME
+      typedef _ArcFaceResNet ArcFaceResNet; //
+      typedef _GlintCosFace GlintCosFace; //
+      typedef _GlintPartialFC GlintPartialFC;
+      typedef _FaceNet FaceNet;
+      typedef _FocalArcFace FocalArcFace;
+      typedef _FocalAsiaArcFace FocalAsiaArcFace;
+      typedef _TencentCurricularFace TencentCurricularFace;
+      typedef _TencentCifpFace TencentCifpFace;
+      typedef _CenterLossFace CenterLossFace;
+      typedef _SphereFace SphereFace;
+#endif
+
+    }
+    // 5. segmentation
+    namespace segmentation
+    {
+#ifdef BACKEND_ONNXRUNTIME
+      typedef _DeepLabV3ResNet101 DeepLabV3ResNet101;
+      typedef _FCNResNet101 FCNResNet101;
+#endif
+
+    }
+    // 6. reid
+    namespace reid
+    {
+#ifdef BACKEND_ONNXRUNTIME
+#endif
+    }
+
+    // 7. ocr
+    namespace ocr
+    {
+#ifdef BACKEND_ONNXRUNTIME
+#endif
+    }
+    // 8. neural rendering
+    namespace render
+    {
+#ifdef BACKEND_ONNXRUNTIME
+#endif
+    }
+    // 9. style transfer
+    namespace style
+    {
+#ifdef BACKEND_ONNXRUNTIME
+      typedef _FastStyleTransfer FastStyleTransfer;
+#endif
+    }
+
+    // 10. colorization
+    namespace colorization
+    {
+#ifdef BACKEND_ONNXRUNTIME
+      typedef _Colorizer Colorizer;
+#endif
+    }
+    // 11. super resolution
+    namespace resolution
+    {
+#ifdef BACKEND_ONNXRUNTIME
+      typedef _SubPixelCNN SubPixelCNN;
+#endif
+    }
+    // 12. image & face & human matting
+    namespace matting
+    {
+#ifdef BACKEND_ONNXRUNTIME
+#endif
+    }
+
+  }
+
+  namespace asr
+  {
+#ifdef BACKEND_ONNXRUNTIME
+#endif
+  }
+
+  namespace nlp
+  {
+#ifdef BACKEND_ONNXRUNTIME
+#endif
+  }
+}
+
+// ONNXRuntime version
+namespace lite
+{
+  namespace onnxruntime
+  {
+    namespace cv
+    {
+      namespace utils = ortcv::utils;
+      namespace types = ortcv::types;
+
+      typedef ortcv::FSANet _ONNXFSANet;
+      typedef ortcv::PFLD _ONNXPFLD;
+      typedef ortcv::UltraFace _ONNXUltraFace;
+      typedef ortcv::AgeGoogleNet _ONNXAgeGoogleNet;
+      typedef ortcv::GenderGoogleNet _ONNXGenderGoogleNet;
+      typedef ortcv::EmotionFerPlus _ONNXEmotionFerPlus;
+      typedef ortcv::VGG16Age _ONNXVGG16Age;
+      typedef ortcv::VGG16Gender _ONNXVGG16Gender;
+      typedef ortcv::SSRNet _ONNXSSRNet;
+      typedef ortcv::FastStyleTransfer _ONNXFastStyleTransfer;
+      typedef ortcv::ArcFaceResNet _ONNXArcFaceResNet;
+      typedef ortcv::Colorizer _ONNXColorizer;
+      typedef ortcv::SubPixelCNN _ONNXSubPixelCNN;
+      typedef ortcv::YoloV4 _ONNXYoloV4;
+      typedef ortcv::YoloV3 _ONNXYoloV3;
+      typedef ortcv::YoloV5 _ONNXYoloV5;
+      typedef ortcv::EfficientNetLite4 _ONNXEfficientNetLite4;
+      typedef ortcv::ShuffleNetV2 _ONNXShuffleNetV2;
+      typedef ortcv::TinyYoloV3 _ONNXTinyYoloV3;
+      typedef ortcv::SSD _ONNXSSD;
+      typedef ortcv::SSDMobileNetV1 _ONNXSSDMobileNetV1;
+      typedef ortcv::DeepLabV3ResNet101 _ONNXDeepLabV3ResNet101;
+      typedef ortcv::DenseNet _ONNXDenseNet;
+      typedef ortcv::FCNResNet101 _ONNXFCNResNet101;
+      typedef ortcv::GhostNet _ONNXGhostNet;
+      typedef ortcv::HdrDNet _ONNXHdrDNet;
+      typedef ortcv::IBNNet _ONNXIBNNet;
+      typedef ortcv::MobileNetV2 _ONNXMobileNetV2;
+      typedef ortcv::ResNet _ONNXResNet;
+      typedef ortcv::ResNeXt _ONNXResNeXt;
+      typedef ortcv::GlintCosFace _ONNXGlintCosFace;
+      typedef ortcv::GlintPartialFC _ONNXGlintPartialFC;
+      typedef ortcv::FaceNet _ONNXFaceNet;
+      typedef ortcv::FocalArcFace _ONNXFocalArcFace;
+      typedef ortcv::FocalAsiaArcFace _ONNXFocalAsiaArcFace;
+      typedef ortcv::TencentCifpFace _ONNXTencentCifpFace;
+      typedef ortcv::TencentCurricularFace _ONNXTencentCurricularFace;
+      typedef ortcv::CenterLossFace _ONNXCenterLossFace;
+      typedef ortcv::SphereFace _ONNXSphereFace;
+
+      // 1. classification
+      namespace classification
+      {
+        typedef _ONNXEfficientNetLite4 EfficientNetLite4;
+        typedef _ONNXShuffleNetV2 ShuffleNetV2;
+        typedef _ONNXDenseNet DenseNet;
+        typedef _ONNXGhostNet GhostNet;
+        typedef _ONNXHdrDNet HdrDNet;
+        typedef _ONNXIBNNet IBNNet;
+        typedef _ONNXMobileNetV2 MobileNetV2;
+        typedef _ONNXResNet ResNet;
+        typedef _ONNXResNeXt ResNeXt;
+      }
+
+      // 2. general object detection
+      namespace detection
+      {
+        typedef _ONNXYoloV3 YoloV3;
+        typedef _ONNXYoloV4 YoloV4;
+        typedef _ONNXYoloV5 YoloV5;
+        typedef _ONNXTinyYoloV3 TinyYoloV3;
+        typedef _ONNXSSD SSD;
+        typedef _ONNXSSDMobileNetV1 SSDMobileNetV1;
+      }
+      // 3. face detection & facial attributes detection
+      namespace face
+      {
+        typedef _ONNXFSANet FSANet; // head pose estimation.
+        typedef _ONNXUltraFace UltraFace;  // face detection.
+        typedef _ONNXPFLD PFLD; // facial landmarks detection.
+        typedef _ONNXAgeGoogleNet AgeGoogleNet; // age estimation
+        typedef _ONNXGenderGoogleNet GenderGoogleNet; // gender estimation
+        typedef _ONNXVGG16Age VGG16Age; // age estimation
+        typedef _ONNXVGG16Gender VGG16Gender; // gender estimation
+        typedef _ONNXEmotionFerPlus EmotionFerPlus; // emotion detection
+        typedef _ONNXSSRNet SSRNet; // age estimation
+
+        namespace detect
+        {
+          typedef _ONNXUltraFace UltraFace;  // face detection.
+        }
+
+        namespace align
+        {
+          typedef _ONNXPFLD PFLD; // facial landmarks detection.
+        }
+
+        namespace pose
+        {
+          typedef _ONNXFSANet FSANet; // head pose estimation.
+        }
+
+        namespace attr
+        {
+          typedef _ONNXAgeGoogleNet AgeGoogleNet; // age estimation
+          typedef _ONNXGenderGoogleNet GenderGoogleNet; // gender estimation
+          typedef _ONNXVGG16Age VGG16Age; // age estimation
+          typedef _ONNXVGG16Gender VGG16Gender; // gender estimation
+          typedef _ONNXEmotionFerPlus EmotionFerPlus; // emotion detection
+          typedef _ONNXSSRNet SSRNet; // age estimation
+        }
+      }
+      // 4. face recognition
+      namespace faceid
+      {
+        typedef _ONNXArcFaceResNet ArcFaceResNet; //
+        typedef _ONNXGlintCosFace GlintCosFace; //
+        typedef _ONNXGlintPartialFC GlintPartialFC;
+        typedef _ONNXFaceNet FaceNet;
+        typedef _ONNXFocalArcFace FocalArcFace;
+        typedef _ONNXFocalAsiaArcFace FocalAsiaArcFace;
+        typedef _ONNXTencentCifpFace TencentCifpFace;
+        typedef _ONNXTencentCurricularFace TencentCurricularFace;
+        typedef _ONNXCenterLossFace CenterLossFace;
+        typedef _ONNXSphereFace SphereFace;
+      }
+      // 5. segmentation
+      namespace segmentation
+      {
+        typedef _ONNXDeepLabV3ResNet101 DeepLabV3ResNet101;
+        typedef _ONNXFCNResNet101 FCNResNet101;
+
+      }
+      // 6. reid
+      namespace reid
+      {
+
+      }
+
+      // 7. ocr
+      namespace ocr
+      {
+
+      }
+      // 8. neural rendering
+      namespace render
+      {
+
+      }
+      // 9. style transfer
+      namespace style
+      {
+        typedef _ONNXFastStyleTransfer FastStyleTransfer;
+      }
+
+      // 10. colorization
+      namespace colorization
+      {
+        typedef _ONNXColorizer Colorizer;
+      }
+      // 11. super resolution
+      namespace resolution
+      {
+        typedef _ONNXSubPixelCNN SubPixelCNN;
+      }
+      // 12. image & face & human matting
+      namespace matting
+      {
+
+      }
+
+    }
+
+    namespace asr
+    {
+
+    }
+
+    namespace nlp
+    {
+    }
+  }
+}
+
+// MNN version
+namespace lite
+{
+  namespace mnn
+  {
+  }
+}
+
+// NCNN version
+namespace lite
+{
+  namespace ncnn
+  {
+  }
+}
+
+// TNN version
+namespace lite
+{
+  namespace tnn
+  {
+  }
+}
+#endif //LITE_AI_LITE_H
---
+++ macos/lite.ai/include/ort/core/ort_core.h
@@ -0,0 +1,83 @@
+//
+// Created by DefTruth on 2021/3/18.
+//
+
+#ifndef LITE_AI_ORT_ORT_CORE_H
+#define LITE_AI_ORT_ORT_CORE_H
+
+#include "ort_config.h"
+#include "ort_handler.h"
+#include "ort_types.h"
+
+namespace ortcv
+{
+  class LITE_EXPORTS FSANet;                     // [0] * reference: https://github.com/omasaht/headpose-fsanet-pytorch
+  class LITE_EXPORTS PFLD;                       // [1] * reference: https://github.com/Hsintao/pfld_106_face_landmarks
+  class LITE_EXPORTS UltraFace;                  // [2] * reference: https://github.com/Linzaer/Ultra-Light-Fast-Generic-Face-Detector-1MB
+  class LITE_EXPORTS AgeGoogleNet;               // [3] * reference: https://github.com/onnx/models/tree/master/vision/body_analysis/age_gender
+  class LITE_EXPORTS GenderGoogleNet;            // [4] * reference: https://github.com/onnx/models/tree/master/vision/body_analysis/age_gender
+  class LITE_EXPORTS EmotionFerPlus;             // [5] * reference: https://github.com/onnx/models/blob/master/vision/body_analysis/emotion_ferplus
+  class LITE_EXPORTS VGG16Age;                   // [6] * reference: https://github.com/onnx/models/tree/master/vision/body_analysis/age_gender
+  class LITE_EXPORTS VGG16Gender;                // [7] * reference: https://github.com/onnx/models/tree/master/vision/body_analysis/age_gender
+  class LITE_EXPORTS SSRNet;                     // [8] * reference: https://github.com/oukohou/SSR_Net_Pytorch
+  class LITE_EXPORTS FastStyleTransfer;          // [9] * reference: https://github.com/onnx/models/blob/master/vision/style_transfer/fast_neural_style
+  class LITE_EXPORTS ArcFaceResNet;              // [10] * reference: https://github.com/deepinsight/insightface/tree/master/recognition/arcface_torch
+  class LITE_EXPORTS Colorizer;                  // [11] * reference: https://github.com/richzhang/colorization
+  class LITE_EXPORTS SubPixelCNN;                // [12] * reference: https://github.com/niazwazir/SUB_PIXEL_CNN
+  class LITE_EXPORTS YoloV4;                     // [13] * reference: https://github.com/argusswift/YOLOv4-pytorch
+  class LITE_EXPORTS YoloV5;                     // [14] * reference: https://github.com/ultralytics/yolov5
+  class LITE_EXPORTS YoloV3;                     // [15] * reference: https://github.com/onnx/models/blob/master/vision/object_detection_segmentation/yolov3
+  class LITE_EXPORTS EfficientNetLite4;          // [16] * reference: https://github.com/onnx/models/blob/master/vision/classification/efficientnet-lite4
+  class LITE_EXPORTS ShuffleNetV2;               // [17] * reference: https://github.com/onnx/models/blob/master/vision/classification/shufflenet
+  class LITE_EXPORTS TinyYoloV3;                 // [18] * reference: https://github.com/onnx/models/blob/master/vision/object_detection_segmentation/tiny-yolov3
+  class LITE_EXPORTS SSD;                        // [19] * reference: https://github.com/onnx/models/blob/master/vision/object_detection_segmentation/ssd
+  class LITE_EXPORTS SSDMobileNetV1;             // [20] * reference: https://github.com/onnx/models/blob/master/vision/object_detection_segmentation/ssd-mobilenetv1
+  class LITE_EXPORTS DeepLabV3ResNet101;         // [21] * reference: https://pytorch.org/hub/pytorch_vision_deeplabv3_resnet101/
+  class LITE_EXPORTS DenseNet;                   // [22] * reference: https://pytorch.org/hub/pytorch_vision_densenet/
+  class LITE_EXPORTS FCNResNet101;               // [23] * reference: https://pytorch.org/hub/pytorch_vision_fcn_resnet101/
+  class LITE_EXPORTS GhostNet;                   // [24] * reference：https://pytorch.org/hub/pytorch_vision_ghostnet/
+  class LITE_EXPORTS HdrDNet;                    // [25] * reference: https://pytorch.org/hub/pytorch_vision_hardnet/
+  class LITE_EXPORTS IBNNet;                     // [26] * reference: https://pytorch.org/hub/pytorch_vision_ibnnet/
+  class LITE_EXPORTS MobileNetV2;                // [27] * reference: https://pytorch.org/hub/pytorch_vision_mobilenet_v2/
+  class LITE_EXPORTS ResNet;                     // [28] * reference: https://pytorch.org/hub/pytorch_vision_resnet/
+  class LITE_EXPORTS ResNeXt;                    // [29] * reference: https://pytorch.org/hub/pytorch_vision_resnext/
+  class LITE_EXPORTS GlintCosFace;               // [30] * reference: https://github.com/deepinsight/insightface/tree/master/recognition/arcface_torch
+  class LITE_EXPORTS GlintPartialFC;             // [31] * reference: https://github.com/deepinsight/insightface/tree/master/recognition/partial_fc
+  class LITE_EXPORTS FaceNet;                    // [32] * reference: https://github.com/timesler/facenet-pytorch
+  class LITE_EXPORTS FocalArcFace;               // [33] * reference: https://github.com/ZhaoJ9014/face.evoLVe.PyTorch
+  class LITE_EXPORTS FocalAsiaArcFace;           // [34] * reference: https://github.com/ZhaoJ9014/face.evoLVe.PyTorch
+  class LITE_EXPORTS TencentCurricularFace;      // [35] * reference: https://github.com/Tencent/TFace/tree/master/tasks/distfc
+  class LITE_EXPORTS TencentCifpFace;            // [36] * reference: https://github.com/Tencent/TFace/tree/master/tasks/cifp
+  class LITE_EXPORTS CenterLossFace;             // [37] * reference: https://github.com/louis-she/center-loss.pytorch
+  class LITE_EXPORTS SphereFace;                 // [38] * reference: https://github.com/clcarwin/sphereface_pytorch
+  class LITE_EXPORTS DREAMFace;                  // [39] reference: https://github.com/penincillin/DREAM
+  class LITE_EXPORTS MobileFaceNet;              // [40] reference: https://github.com/Xiaoccer/MobileFaceNet_Pytorch
+  class LITE_EXPORTS CavaGhostArcFace;           // [41] reference: https://github.com/cavalleria/cavaface.pytorch
+  class LITE_EXPORTS CavaCombinedFace;           // [42] reference: https://github.com/cavalleria/cavaface.pytorch
+  class LITE_EXPORTS CategoryFaceChannel;        // [43] reference: https://github.com/pablovin/FaceChannel
+  class LITE_EXPORTS DemensionFaceChannel;       // [44] reference: https://github.com/pablovin/FaceChannel
+}
+
+namespace ortnlp
+{
+  class LITE_EXPORTS TextCNN; // todo
+  class LITE_EXPORTS ChineseBert; // todo
+  class LITE_EXPORTS ChineseOCRLite; // todo
+}
+
+namespace ortcv
+{
+  using core::BasicOrtHandler;
+  using core::BasicMultiOrtHandler;
+}
+namespace ortnlp
+{
+  using core::BasicOrtHandler;
+  using core::BasicMultiOrtHandler;
+}
+namespace ortasr
+{
+  using core::BasicOrtHandler;
+  using core::BasicMultiOrtHandler;
+}
+#endif //LITE_AI_ORT_ORT_CORE_H
